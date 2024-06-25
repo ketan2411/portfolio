d7ee2123c462db46bcdc6ef71d233f3e2f924da2
ketan-sharmaa <86145791+ketan-sharmaa@users.noreply.github.com>
2024-06-25 23:24:37 +0000

    Deploying to gh-pages from @ ketan2411/portfolio@c1ef050043a3fbf67f408d007dbf91f3d3ac1653 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2403021522"
+    serviceWorkerVersion: "2663381176"
   }
 });
