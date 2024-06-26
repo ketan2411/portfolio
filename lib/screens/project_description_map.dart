fb92474696ba84308471779e8328ea8168afd5c7
ketan-sharmaa <ketan.sharma@softedge.in>
2024-06-26 08:06:19 +0530

    fox

--- lib/screens/project_description_map.dart
+++ lib/screens/project_description_map.dart
@@ -144,7 +144,7 @@ class ProjectDescriptionMAP extends StatelessWidget {
                   height: 50.h,
                 ),
                 Image.asset(
-                  '/mockups/phone.png',
+                  'mockups/phone.png',
                   height: 50.h,
                 ),
               ],
@@ -163,7 +163,7 @@ class ProjectDescriptionMAP extends StatelessWidget {
                     height: size.height,
                   ),
                   Image.asset(
-                    '/mockups/laptop.png',
+                    '/asset/mockups/laptop.png',
                     height: size.height,
                   ),
                 ],
