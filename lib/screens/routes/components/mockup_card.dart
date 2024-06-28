7ea42650cc2eeeff5cc079456213914b1659ed57
ketan-sharmaa <ketan.sharma@softedge.in>
2024-06-28 11:46:29 +0530

    test

--- lib/screens/routes/components/mockup_card.dart
+++ lib/screens/routes/components/mockup_card.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'dart:developer';
 import 'dart:math' as math;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:sensors_plus/sensors_plus.dart';
@@ -40,21 +41,23 @@ class _MockupCardState extends State<MockupCard> {
 
   void initState() {
     super.initState();
-    _streamSubscription = gyroscopeEvents.listen((event) {
-      // Process gyroscope data (x, y, z values)
-      setState(() {
-        // my idea of using cos for this eqution
-        final normalizedX = event.x / 300;
-        final normalizedY = event.y / 600;
-        final value =
-            math.cos(normalizedX * math.pi); // Adjust PI for desired curve
-        final value1 =
-            -math.cos(normalizedY * math.pi); // Adjust PI for desired curve
-        tiltX = value * hoverSensitivity; // Adjust tilt based on value
-        tiltY = (value1 * hoverSensitivity); // Adjust tilt based on value
-        data = " y:$tiltY";
+    if (kIsWeb) {
+      _streamSubscription = gyroscopeEvents.listen((event) {
+        // Process gyroscope data (x, y, z values)
+        setState(() {
+          // my idea of using cos for this eqution
+          final normalizedX = event.x / 300;
+          final normalizedY = event.y / 600;
+          final value =
+              math.cos(normalizedX * math.pi); // Adjust PI for desired curve
+          final value1 =
+              -math.cos(normalizedY * math.pi); // Adjust PI for desired curve
+          tiltX = value * hoverSensitivity; // Adjust tilt based on value
+          tiltY = (value1 * hoverSensitivity); // Adjust tilt based on value
+          data = " y:$tiltY";
+        });
       });
-    });
+    }
   }
 
   @override
@@ -62,11 +65,21 @@ class _MockupCardState extends State<MockupCard> {
     super.dispose();
     _streamSubscription?.cancel(); // Cancel subscription when done
   }
-
+bool scrollOffAndTiltOn=false;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onPanUpdate: (details) {
+      onLongPressStart: (details) {
+        setState(() {
+          scrollOffAndTiltOn=true;
+        });
+      },
+       onLongPressEnd: (details) {
+        setState(() {
+          scrollOffAndTiltOn=false;
+        });
+      },
+      onLongPressMoveUpdate: (details) {
         // Calculate relative mouse position within the widget
         final RenderBox box = context.findRenderObject() as RenderBox;
 
@@ -91,7 +104,6 @@ class _MockupCardState extends State<MockupCard> {
         onHover: (event) {
           // Calculate relative mouse position within the widget
           final RenderBox box = context.findRenderObject() as RenderBox;
-
           final widgetWidth = box.size.width;
           final widgetHeight = box.size.height;
           final localPosition = box.globalToLocal(event.position);
